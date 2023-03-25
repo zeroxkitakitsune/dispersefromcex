a35d6c5eb05f62b2df7fcf6f6c6c0dbaeb2ae7df
kitakitsune <kitakitsune@skiff.com>
2023-03-25 19:00:59 -0400

    Disperse from CEX

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # dispersefromcex
-Withdraw from CEX to many wallets
+Withdraw from CEX to wallets
+## Usage
+1. Clone this repository and run
+```
+$ pip install -r requirements.txt
+```
+2. `wallets.json` is an example of how your wallets file should look like
+3. Enter your API Keys in `dispersefromcex.py` file
+4. Run 
+```
+$ python3 dispersefromcex.py
+```
---
+++ dispersefromcex.py
@@ -0,0 +1,116 @@
+import ccxt
+from config import *
+from ccxt.base.errors import InvalidAddress, InvalidOrder, ExchangeError
+from termcolor import cprint
+import json
+
+#Add proxy
+json_file_path = WALLETS_PATH
+
+account_kucoin = ccxt.kucoin({
+    'apiKey': KUCOIN_API_KEY,
+    'secret': KUCOIN_SECRET,
+    'password': KUCOIN_PASSWORD,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'spot'
+    }
+})
+
+account_binance = ccxt.binance({
+    'apiKey': BINANCE_API_KEY,
+    'secret': BINANCE_API_SECRET,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'spot'
+    }
+})
+
+account_mexc = ccxt.mexc({
+    'apiKey': MEXC_API_KEY,
+    'secret': MEXC_API_SECRET,
+    'enableRateLimit': True,
+    'options': {
+        'defaultType': 'spot'
+    }
+})
+
+def withdraw_kucoin(wallet, amount, code, network):
+    global account_kucoin
+    try:
+        account_kucoin.withdraw(
+            code = code,
+            amount = amount,
+            address = wallet,
+            tag = None, 
+            params = {
+                "network": network
+            }
+        )
+        cprint(f">>> Success | {wallet} | {amount}", "green")
+    except ExchangeError as error:
+        cprint(f">>> Failure | {wallet} | error : {error}", "red")
+
+def withdraw_binance(wallet, amount, code, network):
+    global account_binance
+    try:
+        account_binance.withdraw(
+            code = code,
+            amount = amount,
+            address = wallet,
+            tag = None, 
+            params = {
+                "network": network
+            }
+        )
+        cprint(f">>> Success | {wallet} | {amount}", "green")
+    except ExchangeError as error:
+        cprint(f">>> Failure | {wallet} | error : {error}", "red")
+
+def withdraw_mexc(wallet, amount, code, network):
+    global account_mexc
+    try:
+        account_mexc.withdraw(
+            code = code,
+            amount = amount,
+            address = wallet,
+            tag = None, 
+            params = {
+                "network": network
+            }
+        )
+        cprint(f">>> Success | {wallet} | {amount}", "green")
+    except ExchangeError as error:
+        cprint(f">>> Failure | {wallet} | error : {error}", "red")
+
+def __main__():
+
+    with open(json_file_path, 'r') as j:
+        wallets = json.loads(j.read())
+
+    print('Enter currency code:')
+    code = input()
+
+    print('Enter amount:')
+    amount = input()
+
+    print('Enter network:')
+    network = input()
+
+    print('From what CEX you want to withdraw? (MEXC, Binance, KuCoin)')
+    cex_choice = input()
+
+    if(cex_choice.lower() == 'binance'):
+        for wallet in wallets:
+            withdraw_binance(wallet['address'], amount, code, network)
+    elif(cex_choice.lower() == 'mexc'):
+        for wallet in wallets:
+            withdraw_mexc(wallet['address'], amount, code, network)
+    elif(cex_choice.lower() == 'kucoin'):
+        for wallet in wallets:
+            withdraw_kucoin(wallet['address'], amount, code, network)
+    else:
+        __main__()
+
+if __name__ == '__main__':
+    __main__()
